6dd2aa2d27fcc789bed871ec59abcb6b30a7de0e
HyxiaoGe <18889592303@163.com>
2024-01-03 23:59:11 +0800

    add http server

---
+++ src/main/java/com/hyxiao/http/HttpServerHandler.java
@@ -0,0 +1,36 @@
+package com.hyxiao.http;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+public class HttpServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) msg;
+
+            // 创建响应内容
+            String content = "<h1>Hello, Netty!</h1>";
+
+            FullHttpResponse response = new DefaultFullHttpResponse(request.protocolVersion(), HttpResponseStatus.OK, Unpooled.copiedBuffer(content, CharsetUtil.UTF_8));
+
+            // 设置响应头
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+
+            //  将响应写回客户端，并关闭连接
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
