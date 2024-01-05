6323385fe4eaa2a54bc2a414143ec68d95bf78b5
HyxiaoGe <18889592303@163.com>
2024-01-05 22:18:03 +0800

    project structure adjust

---
+++ src/main/java/com/hyxiao/basic/NettyServer.java
@@ -0,0 +1,44 @@
+package com.hyxiao.basic;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyServer {
+
+    /**
+     * 自动绑定递增端口
+     * @param serverBootstrap
+     * @param port
+     */
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+                bind(serverBootstrap, port + 1);
+            }
+        });
+    }
+
+
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
+            protected void initChannel(NioSocketChannel ch) {
+                //  负责读取客户端来的数据
+                ch.pipeline().addLast(new ServerHandler());
+            }
+        });
+        bind(serverBootstrap, 8000);
+    }
+}
+
+
